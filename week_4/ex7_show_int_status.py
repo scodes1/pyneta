6a88ca97a8f047a1b212746f213757e8e1ee1d9b
My Name <email@email.com>
2022-01-10 13:41:50 -0800

    adding week 4

---
+++ week_4/ex7_show_int_status.py
@@ -0,0 +1,23 @@
+from pprint import pprint
+import textfsm
+
+template_file = "ex2_show_int_status.template"
+template = open(template_file)
+
+with open("ex1_show_int_status.txt") as f:
+    raw_text_data = f.read()
+
+# The argument 'template' is a file handle and 'raw_text_data' is a string.
+re_table = textfsm.TextFSM(template)
+data = re_table.ParseText(raw_text_data)
+template.close()
+
+table_keys = re_table.header
+final_list = []
+for fsm_list in data:
+    fsm_dict = dict(zip(table_keys, fsm_list))
+    final_list.append(fsm_dict)
+
+print()
+pprint(final_list)
+print()
