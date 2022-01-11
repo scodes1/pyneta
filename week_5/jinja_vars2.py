7127eac2a318d4d545b68fff908b52af888e469b
My Name <email@email.com>
2022-01-11 02:06:35 -0800

    adding week 5

---
+++ week_5/jinja_vars2.py
@@ -0,0 +1,12 @@
+from jinja2 import Template
+
+bgp_config = """
+router bgp {{ bgp_as }}
+ bgp router-id {{ router_id }}
+ bgp log-neighbor-changes
+ neighbor {{ peer1 }} remote-as 44
+"""
+
+j2_template = Template(bgp_config)
+output = j2_template.render(bgp_as=22, router_id="1.1.1.1", peer1="10.20.30.1")
+print(output)
